06c704ab3e7b35e9963be8addfb14c6616d96e5a
SumarokovDaniil <44470355+SumarokovDaniil@users.noreply.github.com>
2020-11-12 12:35:54 +0300

    Update ft_join.py

--- ft_join.py
+++ ft_join.py
@@ -1,15 +1,15 @@
 def ft_len(string):
     count = 0
-    for i in string:
+    for _ in string:
         count += 1
     return count
 
 
-def ft_join(lst, sep=' '):
+def ft_join(lst, separator=' '):
     res = ''
     for i in range(ft_len(lst)):
         if i == ft_len(lst) - 1:
             res += lst[i]
         else:
-            res += lst[i] + sep
+            res += lst[i] + separator
     return res
